6491f31ea63a64421c50559de2318334f82ccb8d
Emmanuel <bergerat62@gmail.com>
2026-01-07 17:09:47 -0500

    Multi-silo comments added

--- src/voting-app-k8s/Program.cs
+++ src/voting-app-k8s/Program.cs
@@ -1,30 +1,39 @@
+using Orleans.Configuration;
 using Orleans.Dashboard;
 using Voting.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseOrleans((ctx, orleansBuilder) =>
+builder.Host.UseOrleans((ctx, siloBuilder) =>
 {
     if (ctx.HostingEnvironment.IsDevelopment())
     {
         // During development time, we don't want to have to deal with
         // storage emulators or other dependencies. Just "Hit F5" to run.
-        orleansBuilder
+        siloBuilder
             .UseLocalhostClustering()
             .AddMemoryGrainStorage("votes");
     }
     else
     {
-        // In Kubernetes, we use environment variables and the pod manifest
-        orleansBuilder.UseKubernetesHosting();
+        // In Kubernetes, the default UseKubernetesHosting() uses environment variables and the pod manifest to populate ClusterId and ServiceId from the following environment variables: ORLEANS_SERVICE_ID & ORLEANS_CLUSTER_ID, which can be bound to the pod labels.
+        // It can also be customize as needed, for example, to use a specific Cluster and Service IDs:
+        //siloBuilder.Configure<ClusterOptions>(options =>
+        //{
+        //    options.ClusterId = Environment.GetEnvironmentVariable("ORLEANS_CLUSTER_ID") ?? "votingapp-cluster";
+        //    options.ServiceId = Environment.GetEnvironmentVariable("ORLEANS_SERVICE_ID") ?? "votingapp-service";
+        //});
+
+        // This enables Kubernetes membership & networking integration
+        siloBuilder.UseKubernetesHosting();
 
         // Use Redis for clustering & persistence
         var redisAddress = $"{Environment.GetEnvironmentVariable("REDIS")}:6379";
-        orleansBuilder.UseRedisClustering(options => options.ConfigurationOptions = StackExchange.Redis.ConfigurationOptions.Parse(redisAddress));
-        orleansBuilder.AddRedisGrainStorage("votes", options => options.ConfigurationOptions = StackExchange.Redis.ConfigurationOptions.Parse(redisAddress));
+        siloBuilder.UseRedisClustering(options => options.ConfigurationOptions = StackExchange.Redis.ConfigurationOptions.Parse(redisAddress));
+        siloBuilder.AddRedisGrainStorage("votes", options => options.ConfigurationOptions = StackExchange.Redis.ConfigurationOptions.Parse(redisAddress));
     }
 
     // Add the dashboard
-    orleansBuilder
+    siloBuilder
         .AddDashboard();
 });
 
